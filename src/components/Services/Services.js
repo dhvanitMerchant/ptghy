88f9c75c9234e186c91d1b6d39978b5326ca9097
mauli-D <maulika26@gmail.com>
2020-05-21 19:28:03 -0400

    services done

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -5,11 +5,24 @@ const Services = props => {
   return (
     <section id="services">
       <div className="wrapper container">
-		<Progress done="70"/>
-</div>
+        <h1 className="text-center">What we Offer?</h1>
+        <div class="row">
+          <div class="col-5 mx-auto">
+            <span className="label">Wedding Photography<Progress done="90"/></span>
+            <span className="label">Pre-Wedding Photography<Progress done="90"/></span>
+            <span className="label">Angagement Photography<Progress done="85"/></span>
+            <span className="label">Event Photography<Progress done="80"/></span>
+            <span className="label">Modelling Photoshoot<Progress done="80"/></span>
+            <span className="label">Baby Photoshoot<Progress done="75"/></span>
+            <span className="label">Meternity Photoshoot<Progress done="75"/></span>
+            <span className="label">Product Photohoot<Progress done="70"/></span>
+          </div>
+          <div class="col-5 mx-auto">
+              <div className="img"></div>
+          </div>
+        </div>
+      </div>
     </section>
   );
-
-  
 }
 export default Services;
