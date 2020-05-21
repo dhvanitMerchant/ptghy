080d96f167dc3c0a738c25bcdec2b941886c6104
mauli-D <maulika26@gmail.com>
2020-05-21 16:17:27 -0400

    temporary changes

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Progress from '../Progress/Progress';
 
 const Services = props => {
-    return (
-        <section id="services">
-          
-        </section>
-      );
+  return (
+    <section id="services">
+      <div className="wrapper container">
+		<Progress done="70"/>
+</div>
+    </section>
+  );
+
+  
 }
 export default Services;
