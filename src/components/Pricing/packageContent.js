8bda7a3167be3cdd2806fb830c6f354763cbd77b
Dhvanit <dhvanit24merchant@gmail.com>
2020-06-12 00:10:20 -0400

    dropdown fixed

--- src/components/Pricing/packageContent.js
+++ src/components/Pricing/packageContent.js
@@ -3,6 +3,7 @@ const packages = [
         packageName: 'Wedding Package 1',
         isAvaialable: true,
         days: 1,
+        imgUrl: "//splashbase.s3.amazonaws.com/getrefe/regular/tumblr_nqune4OGHl1slhhf0o1_1280.jpg",
         services: [
             {
                 service: 'One Photographer '
@@ -25,6 +26,8 @@ const packages = [
         packageName: 'Wedding Package 2',
         isAvaialable: true,
         days: 2,
+        imgUrl: "https://firebasestorage.googleapis.com/v0/b/photography-2404.appspot.com/o/images%2Fkissing.jpg?alt=media&token=528528f1-785d-4b1e-a103-d5ea763985cf",
+
         services: [
             {
                 service: 'One Photographer '
@@ -205,121 +208,7 @@ const packages = [
         ],
         price: '30,000'
     },
-    {
-        packageName: 'Pre-Wedding Package 1',
-        isAvaialable: true,
-        days: 1,
-        services: [
-            {
-                service: 'One Photographer '
-            }, {
-                service: 'One Cinematographer '
-            }, {
-                service: 'Highlight Up To 2-3 Min.'
-            }, {
-                service: 'Up to One Hour Short Film'
-            }, {
-                service: 'Calendar/Mini Book '
-            }, {
-                service: 'One Album Of 250-275 Selected Photos'
-            }, {
-                service: 'All Photos in Softcopy'
-            }
-        ],
-        price: '30,000'
-    },
-    {
-        packageName: 'Pre-Wedding Package 2',
-        isAvaialable: true,
-        days: 1,
-        services: [
-            {
-                service: 'One Photographer '
-            }, {
-                service: 'One Cinematographer '
-            }, {
-                service: 'Highlight Up To 2-3 Min.'
-            }, {
-                service: 'Up to One Hour Short Film'
-            }, {
-                service: 'Calendar/Mini Book '
-            }, {
-                service: 'One Album Of 250-275 Selected Photos'
-            }, {
-                service: 'All Photos in Softcopy'
-            }
-        ],
-        price: '30,000'
-    },
-    {
-        packageName: 'Pre-Wedding Package 3',
-        isAvaialable: true,
-        days: 1,
-        services: [
-            {
-                service: 'One Photographer '
-            }, {
-                service: 'One Cinematographer '
-            }, {
-                service: 'Highlight Up To 2-3 Min.'
-            }, {
-                service: 'Up to One Hour Short Film'
-            }, {
-                service: 'Calendar/Mini Book '
-            }, {
-                service: 'One Album Of 250-275 Selected Photos'
-            }, {
-                service: 'All Photos in Softcopy'
-            }
-        ],
-        price: '30,000'
-    },
-    {
-        packageName: 'Pre-Wedding Package 4',
-        isAvaialable: true,
-        days: 1,
-        services: [
-            {
-                service: 'One Photographer '
-            }, {
-                service: 'One Cinematographer '
-            }, {
-                service: 'Highlight Up To 2-3 Min.'
-            }, {
-                service: 'Up to One Hour Short Film'
-            }, {
-                service: 'Calendar/Mini Book '
-            }, {
-                service: 'One Album Of 250-275 Selected Photos'
-            }, {
-                service: 'All Photos in Softcopy'
-            }
-        ],
-        price: '30,000'
-    },
-    {
-        packageName: 'Pre-Wedding Package 5',
-        isAvaialable: true,
-        days: 1,
-        services: [
-            {
-                service: 'One Photographer '
-            }, {
-                service: 'One Cinematographer '
-            }, {
-                service: 'Highlight Up To 2-3 Min.'
-            }, {
-                service: 'Up to One Hour Short Film'
-            }, {
-                service: 'Calendar/Mini Book '
-            }, {
-                service: 'One Album Of 250-275 Selected Photos'
-            }, {
-                service: 'All Photos in Softcopy'
-            }
-        ],
-        price: '30,000'
-    }
+  
 ];
 
 
